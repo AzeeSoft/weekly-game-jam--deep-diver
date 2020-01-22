d8ff2c12d00e7bc1c1e7bad17e70fa0f6420b6ac
Azee <aziztitu1996@gmail.com>
2020-01-21 20:45:22 -0600

    Shows game info on end screen

---
+++ Assets/Scripts/UI/EndScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour
+{
+    public TextMeshProUGUI coinsText;
+    public TextMeshProUGUI depthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        coinsText.text = $"{DiverModel.Instance.coinsCollected:###,##0}";
+        depthText.text = $"{LevelManager.Instance.curDepth:###,##0} m";
+    }
+}
